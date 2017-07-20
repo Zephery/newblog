8063a6ddfcccdf00f28d42db7dbcb68b31311ebc
Zephery <1570631036@qq.com>
2017-07-20 21:51:08 +0800

    压缩gzip

--- src/main/java/com/myblog/controller/BoardController.java
+++ src/main/java/com/myblog/controller/BoardController.java
@@ -4,15 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Zephery on 2017/6/23.
  */
 @Controller
 public class BoardController {
+//    @RequestMapping("board")
+//    public ModelAndView modelAndView(){
+//        ModelAndView modelAndView=new ModelAndView();
+//        modelAndView.setViewName("board");
+//        return modelAndView;
+//    }
     @RequestMapping("board")
-    public ModelAndView modelAndView(){
-        ModelAndView modelAndView=new ModelAndView();
-        modelAndView.setViewName("board");
-        return modelAndView;
+    public String modelAndView(HttpServletResponse response){
+        response.setHeader("Content-Encoding","gzip");
+        return "board";
     }
 }
---
+++ src/main/java/com/myblog/filter/CacheControlFilter.java
@@ -0,0 +1,37 @@
+package com.myblog.filter;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CacheControlFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+//      resp.setHeader("Expires", "Tue, 03 Jul 2001 06:00:00 GMT");
+//      resp.setDateHeader("Last-Modified", new Date().getTime());
+//      resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+        resp.setHeader("Cache-Control", "max-age=0, private");
+//      resp.setHeader("Pragma", "no-cache");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+
+    }
+}
