01c250b6938e1ed3d9b1e6bfd25615c0bdd935fb
wenzhihuai <1570631036@qq.com>
2018-05-01 15:39:11 +0800

    再次完善

---
+++ src/main/java/com/myblog/util/JmxPortCheck.java
@@ -0,0 +1,42 @@
+package com.myblog.util;
+
+import com.myblog.common.Config;
+import joptsimple.internal.Strings;
+
+import java.net.Socket;
+
+/**
+ * @author wenzhihuai
+ * @since 2018/5/1 15:11
+ */
+public class JmxPortCheck {
+
+    private static boolean getURL(String ip, Integer port) {
+        Socket client;
+        try {
+            client = new Socket(ip, port);
+            client.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static String check() {
+        String ipAndPorts = Config.getProperty("jmx_ip_port");
+        if (!Strings.isNullOrEmpty(ipAndPorts)) {
+            String[] strings = ipAndPorts.split(",");
+            for (String string : strings) {
+                String[] str = string.split(":");
+                if (getURL(str[0], Integer.parseInt(str[1]))) {
+                    return string;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        check();
+    }
+}
